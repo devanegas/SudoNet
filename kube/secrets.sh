f238f0a789c8aa9447f3947907b2e237c4740609
Alex Mickelson <alexmickelson96@gmail.com>
2019-09-02 14:57:18 -0600

    merging

--- kube/secrets.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kubectl create secret generic secret-appsettings \
-    --from-file=$HOME/.microsoft/usersecrets/aspnet-WebApplication1-BFE5B400-A838-4B80-8681-AB46A45E14CC/secrets.json
