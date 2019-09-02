0e55554bdae8a30db122b00a82d695eecde926ec
benny <benny@sudo.snow.edu>
2019-09-02 14:48:02 -0600

    working on secrets and docker automation

--- Sudo/Startup.cs
+++ Sudo/Startup.cs
@@ -49,6 +49,7 @@ namespace Sudo
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
+                .AddJsonFile("/app/secrets/appsettings.secrets.json", optional: true)
                 .AddEnvironmentVariables();
 
 
