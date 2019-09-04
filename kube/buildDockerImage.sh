9cdcf9a5c67cfbf5f80232e74ed29ac891b3b031
alexmickelson <alexmickelson96@gmail.com>
2019-09-03 22:16:34 -0600

    docker

--- kube/buildDockerImage.sh
+++ kube/buildDockerImage.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-docker build -t alexmickelson/sudonet \
-    -f ../Sudo/Dockerfile ..
+docker build -t alexmickelson/sudonet -f ../Sudo/Dockerfile ..
 
 docker push alexmickelson/sudonet
