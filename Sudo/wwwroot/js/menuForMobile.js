0eceb3c87bda8a8b7699802ed04c91b5d147c371
Diego Alejandro Vanegas Zúñiga <dieguitovan98@hotmail.com>
2019-09-24 02:15:04 -0600

    Fixed css partially

---
+++ Sudo/wwwroot/js/menuForMobile.js
@@ -0,0 +1,10 @@
+﻿function myFunction() {
+    var x = document.getElementById("myNav")
+
+    if (x.className === "navigation") {
+        x.className += " responsive";
+    }
+    else {
+        x.className = "navigation"
+    }
+}
